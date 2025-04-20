ebeb67e5301c063058bcbdb86c0372f9fe43eb86
Alexandre Ferreira <shieldforce2@gmail.com>
2025-04-20 19:15:47 -0300

    instalando pacote vlucas/phpdotenv

--- src/InitSystem/Boot.php
+++ src/InitSystem/Boot.php
@@ -7,16 +7,18 @@ class Boot
     public function __construct()
     {
         // Agora você pode acessar assim:
-        $host = $_ENV['DB_HOST'];
-        $user = $_ENV['DB_USER'];
-        $pass = $_ENV['DB_PASS'];
-        $name = $_ENV['DB_NAME'];
+        $host  = $_ENV['DB_HOST'];
+        $user  = $_ENV['DB_USER'];
+        $pass  = $_ENV['DB_PASS'];
+        $name  = $_ENV['DB_NAME'];
+        $teste = $_ENV['TESTE'];
 
         var_dump([
-            "host" => $host,
-            "user" => $user,
-            "pass" => $pass,
-            "name" => $name,
+            "host"  => $host,
+            "user"  => $user,
+            "pass"  => $pass,
+            "name"  => $name,
+            "teste" => $teste,
         ]);
     }
 
