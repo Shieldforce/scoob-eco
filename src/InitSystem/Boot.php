829546aed6837567319114a302f75f3277e8b545
Alexandre Ferreira <shieldforce2@gmail.com>
2025-04-20 19:23:03 -0300

    removendo o .idea

--- src/InitSystem/Boot.php
+++ src/InitSystem/Boot.php
@@ -2,24 +2,35 @@
 
 namespace ScoobEco\InitSystem;
 
+use PDO;
+use PDOException;
+
 class Boot
 {
     public function __construct()
     {
         // Agora você pode acessar assim:
-        $host  = $_ENV['DB_HOST'];
-        $user  = $_ENV['DB_USER'];
-        $pass  = $_ENV['DB_PASS'];
-        $name  = $_ENV['DB_NAME'];
-        $teste = $_ENV['TESTE'];
+        $host    = $_ENV['DB_HOST'];
+        $user    = $_ENV['DB_USER'];
+        $pass    = $_ENV['DB_PASS'];
+        $db      = $_ENV['DB_NAME'];
+        $charset = $_ENV['DB_CHARSET'];
 
-        var_dump([
-            "host"  => $host,
-            "user"  => $user,
-            "pass"  => $pass,
-            "name"  => $name,
-            "teste" => $teste,
-        ]);
+        try {
+            $dsn     = "mysql:host=$host;dbname=$db;charset=$charset";
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+
+            $pdo = new PDO($dsn, $user, $pass, $options);
+
+            echo "✅ Conexão com banco de dados realizada com sucesso!";
+        } catch (PDOException $e) {
+            echo "❌ Erro ao conectar com o banco: " . $e->getMessage();
+            exit;
+        }
     }
 
 }
